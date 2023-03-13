9b6b298181789277b80813de51a8698b10f69c39
Aksel Nordal <aksel.nordal@gmail.com>
2023-03-13 08:51:43 +0100

    reproduce sanity baseurl error (#2)

--- sanitypathtest/sanity.cli.ts
+++ sanitypathtest/sanity.cli.ts
@@ -3,6 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'emt9r934',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  vite: (prevConfig) => {
+    return {...prevConfig, build: {...prevConfig.build, assetsDir: 'mynamespace'}}
+  },
 })
